f789368e50f6687c7ec73f124a3f93345444c2e7
Leonardo Furmann <leohitnerf@gmail.com>
2022-11-09 21:22:16 -0300

    criação de um usuario no banco

---
+++ Locadora/Migrations/20221110001047_MigracaoDiaNove.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoLocadora.Migrations
+{
+    public partial class MigracaoDiaNove : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
